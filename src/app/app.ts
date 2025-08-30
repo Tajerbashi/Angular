f602630829607aebb36bfda091114af1dcc32af6
KTajerbashi <kamrantajerbashi@gmail.com>
2025-08-30 23:04:47 +0330

    #Feature ReArch Solution

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('auth-app');
+  protected readonly title = signal('angular-app');
 }
