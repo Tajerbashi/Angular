9f58a29295621efe0a19bb241ee408ee31c2beae
KTajerbashi <kamrantajerbashi@gmail.com>
2025-08-31 23:05:36 +0330

    #Feature add some components for common used

---
+++ src/app/shared/components/tk-card/tk-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tk-card',
+  imports: [],
+  templateUrl: './tk-card.component.html',
+  styleUrl: './tk-card.component.scss'
+})
+export class TkCardComponent {
+
+}
---
+++ src/app/shared/components/tk-page/tk-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tk-page',
+  imports: [],
+  templateUrl: './tk-page.component.html',
+  styleUrl: './tk-page.component.scss'
+})
+export class TkPageComponent {
+
+}
