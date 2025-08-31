77f2504ff32e90d4eae3a17db093c9554bc79c43
KTajerbashi <kamrantajerbashi@gmail.com>
2025-08-31 23:03:57 +0330

    #Feature add main template

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,39 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { MainLayoutComponent } from './shared/layouts/main-layout/main-layout.component';
+import { AuthLayoutComponent } from './shared/layouts/auth-layout/auth-layout.component';
+import { SignupComponent } from './pages/auth/signup/signup.component';
+import { LoginComponent } from './pages/auth/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  //   { path: 'services', component: ServicesComponent },
+  //   { path: 'contact', component: ContactComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  //   { path: 'dashboard', component: DashboardComponent },
+  { path: '**', redirectTo: '/home' }, // Wildcard route for 404
+];
+
+// export const routes: Routes = [
+//   {
+//     path: '',
+//     component: MainLayoutComponent,
+//     children: [
+//       { path: '', component: HomeComponent },
+//       { path: 'home', component: HomeComponent },
+//       { path: 'about', component: AboutComponent },
+//     ],
+//   },
+//   {
+//     path: 'auth',
+//     component: AuthLayoutComponent,
+//     children: [
+//       { path: 'login', component: LoginComponent },
+//       { path: 'signup', component: SignupComponent },
+//     ],
+//   },
+// ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,38 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { MainLayoutComponent } from './shared/layouts/main-layout/main-layout.component';
+import { AuthLayoutComponent } from './shared/layouts/auth-layout/auth-layout.component';
+// Angular Material Modules
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatDividerModule } from '@angular/material/divider';
+import { TkCardComponent } from './shared/components/tk-card/tk-card.component';
+import { TkPageComponent } from './shared/components/tk-page/tk-page.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    NavbarComponent,
+    FooterComponent,
+    // MainLayoutComponent,
+    // AuthLayoutComponent,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatMenuModule,
+    MatDividerModule,
+    TkPageComponent,
+  ],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('angular-app');
