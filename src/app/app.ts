26ae0d4abb13c70727237b25ef6570741b2f020c
KTajerbashi <kamrantajerbashi@gmail.com>
2025-09-01 20:08:45 +0330

    #Feature Clear Solution

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,39 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from './shared/components/navbar/navbar.component';
-import { FooterComponent } from './shared/components/footer/footer.component';
-import { MainLayoutComponent } from './shared/layouts/main-layout/main-layout.component';
-import { AuthLayoutComponent } from './shared/layouts/auth-layout/auth-layout.component';
-// Angular Material Modules
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatDividerModule } from '@angular/material/divider';
-import { TkCardComponent } from './shared/components/tk-card/tk-card.component';
-import { TkPageComponent } from './shared/components/tk-page/tk-page.component';
+
 @Component({
   selector: 'app-root',
-  imports: [
-    RouterOutlet,
-    NavbarComponent,
-    FooterComponent,
-    // MainLayoutComponent,
-    // AuthLayoutComponent,
-    MatToolbarModule,
-    MatButtonModule,
-    MatIconModule,
-    MatSidenavModule,
-    MatListModule,
-    MatMenuModule,
-    MatDividerModule,
-    TkPageComponent,
-  ],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss',
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('angular-app');
+  protected readonly title = signal('app-tk-angular');
 }
