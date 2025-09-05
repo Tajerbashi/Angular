a1a58f2b606c4ff818c5754f9d75287d1fe3eb29
KTajerbashi <kamrantajerbashi@gmail.com>
2025-09-05 19:44:26 +0330

    #Feature Add Some Configuration for base of project

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,12 +2,14 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SignalIndex } from './components/signal/signal-index/signal-index';
 import { SelectorIndex } from './components/selector/selector-index/selector-index';
+import { DataBindingIndex } from './components/dataBinding/data-binding-index/data-binding-index';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SignalIndex, SelectorIndex],
+  imports: [RouterOutlet, SignalIndex, SelectorIndex, DataBindingIndex],
   templateUrl: './app.html',
   styleUrl: './app.css',
+  standalone: true,
 })
 export class App {
   count = 0;
---
+++ src/shared-imports.ts
@@ -0,0 +1,5 @@
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+// Export a shared array of common imports
+export const SHARED_IMPORTS = [CommonModule, FormsModule];
