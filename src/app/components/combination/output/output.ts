7da6e05e72fe314b2886b3c5fba063eb066ceab2
KTajerbashi <kamrantajerbashi@gmail.com>
2025-09-07 21:34:07 +0330

    #Feature add output

---
+++ src/app/components/combination/output/output.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-output',
+  imports: [],
+  templateUrl: './output.html',
+  styleUrl: './output.css'
+})
+export class Output {
+
+}
