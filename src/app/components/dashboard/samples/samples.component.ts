d979c8210e0e75e86fb7e6aaf5d2ab8a12f52c39
KTajerbashi <kamrantajerbashi@gmail.com>
2025-09-19 13:40:09 +0330

    .

---
+++ src/app/components/dashboard/samples/samples.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-samples',
+  imports: [],
+  templateUrl: './samples.component.html',
+  styleUrl: './samples.component.css'
+})
+export class SamplesComponent {
+
+}
