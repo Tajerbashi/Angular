f9642f6d165fd4611e42129832c998dd1470027c
KTajerbashi <kamrantajerbashi@gmail.com>
2025-10-07 16:41:02 +0330

    #Feature Add Page and grid Components for Bases

---
+++ src/app/components/grid/grid.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-grid',
+  template: ` <div class="app-grid"><ng-content></ng-content></div> `,
+})
+export class GridComponent {}
