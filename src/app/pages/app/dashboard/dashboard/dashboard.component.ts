9f1179bebd4ca15d3ef5abd5136cab6c5e70fcc6
KTajerbashi <kamrantajerbashi@gmail.com>
2025-10-07 16:41:38 +0330

    #Feature Add Dashboard Styles

--- src/app/pages/app/dashboard/dashboard/dashboard.component.ts
+++ src/app/pages/app/dashboard/dashboard/dashboard.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { PageContainerComponent } from '../../../../components/page-container/page-container.component';
+import { PageContentComponent } from '../../../../components/page-content/page-content.component';
+import { GridComponent } from '../../../../components/grid/grid.component';
+import { CardContainerComponent } from '../../../../components/cardContainer/card-container/card-container.component';
+import { CardHeaderComponent } from '../../../../components/cardContainer/card-header/card-header.component';
+import { CardContentComponent } from '../../../../components/cardContainer/card-content/card-content.component';
+import { CardFooterComponent } from '../../../../components/cardContainer/card-footer/card-footer.component';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [
+    PageContainerComponent,
+    PageContentComponent,
+    GridComponent,
+    CardContainerComponent,
+    CardHeaderComponent,
+    CardContentComponent,
+    CardFooterComponent,
+  ],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrl: './dashboard.component.css',
 })
-export class DashboardComponent {
-
-}
+export class DashboardComponent {}
